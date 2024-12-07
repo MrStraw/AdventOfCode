09b4a2e042ec0a7d0cc8f93ad41899b336ec7087
Simon Métais <metais.simon@icloud.com>
2024-12-07 17:57:58 +0100

    more clean

--- 2024/7/7.py
+++ 2024/7/7.py
@@ -1,8 +1,7 @@
 import itertools
-from typing import Iterator
 
 
-def can_be_calibrate(goal: int, numbers: list[int]) -> int:
+def can_be_calibrate(goal: int, numbers: list[int]) -> bool:
     for operators_combinaison in itertools.product('+*|', repeat=len(numbers) - 1):
         nbs = iter(numbers)
         result = next(nbs)
@@ -16,14 +15,15 @@ def can_be_calibrate(goal: int, numbers: list[int]) -> int:
                     result = int(str(result) + str(next(nbs)))
 
         if result == goal:
-            return goal
-    return 0
+            return True
+    return False
 
 
 total = 0
 with open('7.txt') as f:
     for line in f:
         goal_, *numbers_ = [int(i) for i in line.replace(':', '').split()]
-        total += can_be_calibrate(goal_, numbers_)
+        if can_be_calibrate(goal_, numbers_):
+            total += goal_
 
-print(total)  # 159490400628354
+print(total)
