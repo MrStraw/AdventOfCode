8fe4a7cd1b17b730c33f9fb7e19b59514d972792
Simon Métais <metais.simon@icloud.com>
2024-12-08 14:15:17 +0100

    2024 08

---
+++ 2024/8/8.py
@@ -0,0 +1,35 @@
+import itertools
+
+coord = tuple[int, int]
+
+antennas_types: dict[str, set[coord]] = {}
+all_antennas_pos: set[coord] = set()
+with open('8.txt') as f:
+    for y, line in enumerate(f):
+        line = line.rstrip()
+        for x, ant_type in enumerate(line):
+            if ant_type == '.':
+                continue
+            elif ant_type not in antennas_types:
+                antennas_types[ant_type] = set()
+            antennas_types[ant_type].add((x, y))
+            all_antennas_pos.add((x, y))
+map_len = x, y
+
+
+def calcul_antinodes() -> set[coord]:
+    all_antis = set()
+    for antennas_pos in antennas_types.values():
+        for ant_a, ant_b in itertools.product(antennas_pos, repeat=2):
+            if ant_a is ant_b:
+                continue
+            for i in itertools.count(1):
+                anti_x, anti_y = ant_a[0] + (ant_a[0] - ant_b[0]) * i, ant_a[1] + (ant_a[1] - ant_b[1]) * i
+                if not (0 <= anti_x <= map_len[0]) or not (0 <= anti_y <= map_len[1]):
+                    break
+                all_antis.add((anti_x, anti_y))
+    return all_antis | all_antennas_pos
+
+
+antis_ = calcul_antinodes()
+print(len(antis_))
