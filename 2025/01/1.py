ab3af3a1d0d156de189c6e3e8967ffc5b267cfba
SimonSystemU <simon.metais@externe.systeme-u.fr>
2025-12-02 17:58:49 +0100

    01 2025

---
+++ 2025/01/1.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Iterator
+
+
+def instructions() -> Iterator[tuple[str, int]]:
+    file = Path(__file__).parent / 'file.txt'
+    with file.open() as f:
+        for line in f:
+            line = line.strip()
+            yield line[0], int(line[1:])
+
+
+def rotate(start: int, direction: str, value: int) -> int:
+    return (start + (-value if direction == 'L' else value)) % 100
+
+# print(rotate(0, 'L', 101))
+# print(rotate(99, 'R', 1))
+
+step, i = 50, 0
+for d, r in instructions():
+    step = rotate(step, d, r)
+    if step == 0:
+        i += 1
+
+print(i)
---
+++ 2025/01/2.py
@@ -0,0 +1,7 @@
+position, clics = 50, 0
+for instructions in open('prod.txt'):
+    raw_next_pos = position + int(instructions.replace('L', '-').strip('R\n'))
+    clics += abs(raw_next_pos) // 100 + bool(position) if raw_next_pos <= 0 else raw_next_pos // 100
+    position = raw_next_pos % 100
+
+assert clics == 5847
